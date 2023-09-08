21ac73a6d1e01af5c0d2dd4f9db49bc0e55d63fe
sesethu93 <e.tuswa@gmail.com>
2023-09-07 19:48:45 -0700

    puts fuction

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the begining function
+ * Return: when valid return 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return ('0');
+}
