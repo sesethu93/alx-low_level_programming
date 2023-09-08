030e8c5d7dc9d6c8db10673182e71e275e0e41d6
sesethu93 <e.tuswa@gmail.com>
2023-09-07 21:24:32 -0700

    install

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a line using the function printf
+ * Return: 0 if successful
+ */
+
+int main(void)
+
+{
+	printf("size of a char: %lu byte(s)" . sizeof(char));
+	printf("size of an int: %lu byte(s)" . sizeof(int));
+	printf("size of an long int: %lu byte(s)" . sizeof(long int));
+	printf("size of an long long int: %lu byte(s)" . sizeof(long long int));
+	printf("size of an float: %lu byte(s)" . sizeof(float));
+
+	return (0);
+}
