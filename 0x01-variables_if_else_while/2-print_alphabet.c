692cc393fc8d37c3bf23fd8f5077931925afd023
sesethu93 <e.tuswa@gmail.com>
2023-09-08 20:24:02 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main funtion
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+
+}
