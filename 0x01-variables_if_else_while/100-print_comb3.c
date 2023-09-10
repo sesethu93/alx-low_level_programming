80a578632d85cb6efbbd888f3be2d2935f588f89
sesethu93 <e.tuswa@gmail.com>
2023-09-10 09:57:28 -0700

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 
 /**
- * main - main funtion
+ * main - print two digits separated with , and space
  *
- * Return: 0
+ * Return: (0)
+ *
  */
 
 int main(void)
 {
-	int i;
+	int num_1, num_2;
 
-	for (i = 0; i < 10; i++)
-		putchar(i)
-		else
-			putchar(i + "10");
-			putchar(",");
-			putchar(" ");
+	num_1 = '0';
+	num_2 = '0';
+
+	while (num_1 <= '9')
+	{
+		while (num_2 <= '9')
+		{
+			if (num_1 < num_2)
+			{
+				putchar(num_1);
+				putchar(num_2);
+				if (num_1 != '8' || (num_1 == '8' && num_2 != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			num_2++;
+		}
+		num_1++;
+		num_2 = '0';
+	}
+	putchar('\n');
+	return (0);
 }
