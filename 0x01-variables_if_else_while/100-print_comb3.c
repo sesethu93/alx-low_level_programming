d57f2f9b00a4fd788cd37ffc9ce6e34f91b5dcc5
sesethu93 <e.tuswa@gmail.com>
2023-09-10 09:09:31 -0700

    print comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main funtion
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i)
+		else
+			putchar(i + "10");
+			putchar(",");
+			putchar(" ");
+}
