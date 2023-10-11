003a12210df0909d023dd54ca4d6aec6bc2554ac
sesethu93 <e.tuswa@gmail.com>
2023-10-11 08:20:36 -0700

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,13 +1,22 @@
 #include <unistd.h>
+#include "function_pointers.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * array_iterator - prints each array elem on a new line
+ * @array: array
+ * @size: how many elem to print
+ * @action: pointer to print in regular or hex
+ * Return void
  */
-int _putchar(char c)
+
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	return (write(1, &c, 1));
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
