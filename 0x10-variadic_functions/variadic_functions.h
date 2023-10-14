f9bc67bb53761c8cfbbde21dc2323be0bf973024
sesethu93 <e.tuswa@gmail.com>
2023-10-14 06:28:04 -0700

    Task. 0. sum them all

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
